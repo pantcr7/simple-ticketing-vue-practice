19224b0b5339ce8ab4149e02eed1f7eb02c4326b
Prasant <prasantpant141@gmail.com>
2024-07-16 01:37:30 +0545

    ticketdetails modified

--- frontend/src/components/TicketDetails.vue
+++ frontend/src/components/TicketDetails.vue
@@ -1,33 +1,95 @@
 <template>
-    <div v-if="ticketDetails.doc">
-      <div>
-        <h1>
-          {{ ticketDetails.doc.description }}
+  <div v-if="ticketDetails.doc" class="mt-5">
+    <div class="flex justify-between items-center">
+      <div class="flex justify-between gap-2 items-center">
+        <h1 class="text-[20px] cursor-pointer " @click="router.push({'name':'tickets'})">
+          🔙
         </h1>
-        <Badge>{{ ticketDetails.doc.status }}</Badge>
+        <span class="text-[20px] text-gray-600">#{{ route.params.name }}</span>
+        <h1  class="font-bold text-[25px]">{{ ticketDetails.doc.title }}</h1>
+        <Badge
+          :variant="'subtle'"
+          :theme="getStatusColor(ticketDetails.doc.status)"
+          size="sm"
+          :label="ticketDetails.doc.status"/>
       </div>
-      <Button @click="ticketDetails.setValue.submit({ status: 'Closed' })">
-        Mark as Closed
-      </Button>
+      <div>
+        <Button :variant="'solid'" @click="ticketDetails.markAsResolved.submit()" :disabled="ticketDetails.doc.status === 'Resolved'">
+          Mark as Resolved
+        </Button> 
+      </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { useRoute } from 'vue-router';
-  import { createDocumentResource, Button, Badge } from 'frappe-ui';
-  
-  const route = useRoute();
-  
-  let ticketDetails = createDocumentResource({
-    doctype: 'Support Ticket',
-    name: route.params.name,
-  });
-  
-  console.log('ticketDetails:', ticketDetails);
-  
-  const markAsClosed = () => {
-    ticketDetails.setValue({ status: 'Closed' }).then(() => {
-      ticketDetails.reload();
-    });
-  };
-  </script>
+
+    <!-- Divider -->
+    <div class="my-4">
+      <hr class="border-t-2 border-gray-300"/>
+    </div>
+
+    <!-- description & category-->
+    <div class="flex justify-between">
+      <div class="">
+        <h2 class="text-lg mb-3">Description</h2>
+        <strong v-html="ticketDetails.doc.description"></strong>
+      
+      </div>
+      <div class="mr-40 flex justify-end">
+        <!--vertical divider-->
+        <p class="border-l-2 border-gray-300 mx-4 h-24"></p>
+        <div>
+          <h3 class="text-lg font-bold">Category</h3>
+        <Badge class="text-2xl"
+          :variant="'outline'"
+          theme="gray"
+          size="md"
+          :label="ticketDetails.doc.category"
+        />
+        <h3 class="text-lg font-bold">Purchase Date</h3>
+        <p>{{ getFormattedDate(ticketDetails.doc.purchase_date) }}</p>
+        </div>
+       
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+import { createDocumentResource, Button, Badge } from 'frappe-ui';
+import dayjs from 'dayjs';
+
+const route = useRoute();
+const router = useRouter();
+
+let ticketDetails = createDocumentResource({
+  doctype: 'Support Ticket',
+  name: route.params.name,
+  whitelistedMethods: {
+    markAsResolved: 'mark_as_resolved',
+  },
+});
+
+console.log('ticketDetails:', ticketDetails);
+
+// const markAsClosed = () => {
+//   ticketDetails.setValue({ status: 'Closed' }).then(() => {
+//     ticketDetails.reload();
+//   });
+// };
+
+function getFormattedDate(date) {
+  return dayjs(date).format('MMMM DD, YYYY');
+}
+const getStatusColor = (status) => {
+  switch (status) {
+    case 'Open':
+      return 'red';
+    case 'Resolved':
+      return 'blue';
+    case 'Closed':
+      return 'green';
+    default:
+      return 'gray';
+  }
+};
+</script>
--- simple_ticketing_vue_practice/simple_ticketing/doctype/support_ticket/support_ticket.py
+++ simple_ticketing_vue_practice/simple_ticketing/doctype/support_ticket/support_ticket.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2024, pp and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SupportTicket(Document):
-	pass
+	@frappe.whitelist()
+	def mark_as_resolved(self):
+		self.status = "Resolved"
+		self.save()
+
